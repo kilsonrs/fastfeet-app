84e8a119f92d8ba2033c23b8cb2ca274fdc1b09b
Kilson <kilsonrs@gmail.com>
2021-05-22 10:23:27 -0300

    chore: add splashscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import { makeServer } from './fake-server';
 import Routes from './routes';
 import AppProvider from './hooks';
@@ -9,6 +10,9 @@ import AppProvider from './hooks';
 makeServer();
 
 const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#4C33CC" />
